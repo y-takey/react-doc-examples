60dc28b3e302cfebc060838ef735bf9011e4406c
Yuichiro Take <y.takey@gmail.com>
2018-09-13 17:25:00 +0900

    Add `antd`

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 // @flow
 import * as React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Layout from "antd/lib/layout";
 
 import Home from "./pages/Home";
 import Ground from "./pages/Ground";
+import "./App.css";
 
 const App = () => (
   <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/ground">Ground</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/ground" component={Ground} />
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Layout.Sider theme="light">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/ground">Ground</Link>
+          </li>
+        </ul>
+      </Layout.Sider>
+      <Layout>
+        <Layout.Content style={{ margin: "24px 16px 0" }}>
+          <Route exact path="/" component={Home} />
+          <Route path="/ground" component={Ground} />
+        </Layout.Content>
+      </Layout>
+    </Layout>
   </Router>
 );
 
