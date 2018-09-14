035a7d50784c1f127196929370b0d06146900140
Yuichiro Take <y.takey@gmail.com>
2018-09-14 17:54:25 +0900

    Add links to documents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 // @flow
 import * as React from "react";
+import { Row, Col } from "antd";
 
 import PrimaryButton from "../components/buttons/PrimaryButton";
+import LinkButton from "../components/buttons/LinkButton";
+
+const GutterRow = ({ children }) => (
+  <Row gutter={16} style={{ margin: 20 }}>
+    {children}
+  </Row>
+);
 
 const Home = () => (
   <div>
@@ -10,6 +18,22 @@ const Home = () => (
       text="foo"
       onClick={() => console.log("primary button clicked.")}
     />
+    <GutterRow>
+      <Col span={6} offset={6}>
+        <LinkButton text="Storybook" path="http://localhost:9009" />
+      </Col>
+      <Col span={6}>
+        <LinkButton text="Styleguidist" path="http://localhost:9009" />
+      </Col>
+    </GutterRow>
+    <GutterRow>
+      <Col span={6} offset={6}>
+        <LinkButton text="docz" path="http://localhost:9009" />
+      </Col>
+      <Col span={6}>
+        <LinkButton text="Display Window" path="http://localhost:9009" />
+      </Col>
+    </GutterRow>
   </div>
 );
 
